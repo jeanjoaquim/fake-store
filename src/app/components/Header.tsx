39990b3a841972093d19e0e99cc4cb2cbd0c0f06
Jean Joaquim <jeangeeklee@gmail.com>
2024-09-10 23:52:40 -0300

    adds first component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import "../globalicons.css"
+
+export default function Header() {
+    return (
+        <header>
+            <span className="material-symbols-outlined">arrow_back</span>
+        </header>
+    );
+}
