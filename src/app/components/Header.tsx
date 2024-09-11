a2a5eed50af88825135cca25a9ae36c466f716f7
Jean Jean <jeanimodulo@outlook.com>
2024-09-11 18:27:45 -0300

    add styles for header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,20 @@
-import "../globalicons.css"
+import "../globalicons.css";
+import "../globals.css";
+import Image from "next/image";
+import MobileMenu from "./MobileMenu";
 
 export default function Header() {
     return (
-        <header>
-            <span className="material-symbols-outlined">arrow_back</span>
+        <header className="bg-white px-2 py-2 flex justify-between">
+            <div className="flex items-center gap-1">
+                <Image src="/images/logo.svg" width={24} height={24} alt="logo"/>
+                <h5 className="font-bold">Market</h5>
+            </div>
+            <div className="flex items-center">
+                <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                <span className="material-symbols-outlined py-2 px-2">menu</span>
+            </div>
+            <MobileMenu />
         </header>
     );
 }
---
+++ src/app/components/MobileMenu.tsx
@@ -0,0 +1,11 @@
+import "../globalicons.css";
+import "../globals.css";
+import Image from "next/image";
+
+export default function MobileMenu() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
