20b8b18d0d5345c7f0f637d36124f8c1e313ffcb
Jean Joaquim <jeangeeklee@gmail.com>
2024-09-19 20:36:27 -0300

    adds fix for hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen relative">
       <Image className="w-full h-full object-cover brightness-75 grid-item" src="/images/hero.jpg" alt="" height={4000} width={4000} />
-      <div className="absolute bottom-0 mx-3 mb-3 flex flex-col">
+      <div className="absolute bottom-0 mx-3 mb-24 flex flex-col">
         <h5 className="text-3xl text-center font-bold text-white mb-8">Find Everything You Need, All in One Place!</h5>
         <button className="text-white bg-[#FF5900] w-9/12 self-center py-3 rounded-lg shadow">Acessar loja</button>
       </div>
