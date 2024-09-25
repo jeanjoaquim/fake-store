9a071ece97b8b5bfbd347d577662e730268e0353
Jean Joaquim <jeangeeklee@gmail.com>
2024-09-24 23:34:31 -0300

    adds shop page

--- src/app/components/DesktopMenu.tsx
+++ src/app/components/DesktopMenu.tsx
@@ -2,6 +2,8 @@
 import "../globalicons.css";
 import "../globals.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface Props {
     onMenuClick: () => void;
@@ -9,25 +11,38 @@ interface Props {
 
 export default function DesktopMenu({ onMenuClick }: Props) {
 
+    const currentPath = usePathname();
+
     return (
-        <div className="bg-white px-2 py-2 flex justify-between fixed w-full z-10">
+        <div className="bg-white px-2 py-2 sm:px-8 flex justify-between fixed w-full z-10">
             <div className="flex items-center gap-1">
                 <Image src="/images/logo.svg" width={24} height={24} alt="logo"/>
                 <h5 className="font-bold">Market</h5>
             </div>
             <div className="flex items-center">
                 <nav>
-                    <ul className="hidden md:flex mx-8">
-                        <li className="px-8">Home</li>
-                        <li className="px-8">Shop</li>
+                    <ul className="hidden md:flex">
+                        <li className="px-8">
+                            <Link href="/" className={currentPath === "/" ? "text-primaryColor" : ""}>Home</Link>
+                        </li>
+                        <li className="px-8">
+                            <Link href="/shop" className={currentPath === "/shop" ? "text-primaryColor" : ""}>Shop</Link>
+                        </li>
                         <li className="px-8">About</li>
                         <li className="px-8">Contact</li>
                     </ul>
                 </nav>
-                <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                <span className="material-symbols-outlined py-2 px-2 cart-button mx-8">shopping_cart</span>
                 <button className="md:hidden" onClick={onMenuClick}>
                     <span className="material-symbols-outlined py-2 px-2">menu</span>
                 </button>
+                <div className="hidden md:flex gap-2">
+                    <Image className="rounded-full self-center" src="/images/user.jpg" width={32} height={32} alt="user"/>
+                    <div>
+                        <h5 className="text-sm font-bold">Fernando Souza</h5>
+                        <p className="text-xs text-primaryColor leading-3">Sair</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,25 +3,95 @@ import Image from "next/image"
 export default function Shop() {
     return(
         <div className="pt-16 mx-2">
-            <div className="bg-white px-2 rounded-lg">
-                <h5 className="font-bold text-3xl pt-2">Products</h5>
+            <div className="bg-white px-2 rounded-lg max-w-5xl sm:px-8 sm:py-2 mx-auto">
+                <h5 className="font-bold text-3xl pt-2 mb-3">Products</h5>
 
-                <div className="grid grid-cols-2 gap-2">
-                    <div>
-                        <Image src="/images/hero.jpg" alt="" width={200} height={200}/>
-                        <h5>Fjallraven - Foldsack No. 1</h5>
-                        <span>men's clothing</span>
-                        <span>R$300,00</span>
-                        <button>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 pb-2">
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
                             <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
                         </button>
                     </div>
-                    <div>
-                        <Image src="/images/hero.jpg" alt="" width={200} height={200}/>
-                        <h5>Fjallraven - Foldsack No. 1</h5>
-                        <span>men's clothing</span>
-                        <span>R$300,00</span>
-                        <button>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
+                            <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                        </button>
+                    </div>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
+                            <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                        </button>
+                    </div>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
+                            <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                        </button>
+                    </div>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
+                            <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                        </button>
+                    </div>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
+                            <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                        </button>
+                    </div>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
+                            <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
+                        </button>
+                    </div>
+                    <div className="border-solid border border-gray-100 rounded-lg overflow-hidden">
+                        <Image src="/images/product.png" alt="" width={200} height={200}/>
+                        <div className="px-2">
+                            <h5 className="font-bold">Fjallraven - Foldsack No. 1</h5>
+                            <p className="uppercase text-sm text-neutral-300">men's clothing</p>
+                            <p className="text-primaryColor font-bold text-lg">R$300,00</p>
+                        </div>
+                        <button className="bg-primaryColor w-full text-white py-1">
                             <span className="material-symbols-outlined py-2 px-2">shopping_cart</span>
                         </button>
                     </div>
